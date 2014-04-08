70853eab52e1aeb294c9c781ec0cd2ccae4430ab
parallels <parallels@ubuntu.(none)>
2014-04-08 13:20:01 +0200

    Loglevel Implementation, parsing arguments

--- Program/Log-Level.h
+++ Program/Log-Level.h
@@ -1,8 +1,123 @@
-#define LOGLEVEL_EMERGENCY 0
-#define LOGLEVEL_ALERT 1
-#define LOGLEVEL_CRITICAL 2
-#define LOGLEVEL_ERROR 3
-#define LOGLEVEL_WARNING 4
-#define LOGLEVEL_NOTICE 5
-#define LOGLEVEL_INFORMATIONAL 6
+#define LOGLEVEL_ARG "-l";
+
+int setLogLevel(int logLevel) {
+
+	switch (logLevel) {
+	case 7:
+		undefLogLevel();
 #define LOGLEVEL_DEBUG 7
+#define LOGLEVEL_INFORMATIONAL 6
+#define LOGLEVEL_NOTICE 5
+#define LOGLEVEL_WARNING 4
+#define LOGLEVEL_ERROR 3
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_DEBUG);
+		return 1;
+
+	case 6:
+		undefLogLevel();
+#define LOGLEVEL_INFORMATIONAL 6
+#define LOGLEVEL_NOTICE 5
+#define LOGLEVEL_WARNING 4
+#define LOGLEVEL_ERROR 3
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_INFORMATIONAL);
+		return 1;
+
+	case 5:
+		undefLogLevel();
+#define LOGLEVEL_NOTICE 5
+#define LOGLEVEL_WARNING 4
+#define LOGLEVEL_ERROR 3
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_NOTICE);
+		return 1;
+
+	case 4:
+		undefLogLevel();
+#define LOGLEVEL_WARNING 4
+#define LOGLEVEL_ERROR 3
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_WARNING);
+		return 1;
+
+	case 3:
+		undefLogLevel();
+#define LOGLEVEL_ERROR 3
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_ERROR);
+		return 1;
+
+	case 2:
+		undefLogLevel();
+#define LOGLEVEL_CRITICAL 2
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_CRITICAL);
+		return 1;
+
+	case 1:
+		undefLogLevel();
+#define LOGLEVEL_ALERT 1
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_ALERT);
+		return 1;
+
+	case 0:
+		undefLogLevel();
+#define LOGLEVEL_EMERGENCY 0
+		printf("set Log-Level to %i\n", LOGLEVEL_EMERGENCY);
+		return 1;
+
+	default:
+		printf("Argument was not valid. Valid inputs for LOGLEVELS are 0 - 7\n");
+		break;
+	}
+
+}
+
+void undefLogLevel() {
+	printf("Before setting new loglevel, clear old Log-Level.\n");
+#ifdef LOGLEVEL_DEBUG
+#undef LOGLEVEL_DEBUG 7
+#endif
+
+#ifdef LOGLEVEL_INFORMATIONAL
+#undef LOGLEVEL_INFORMATIONAL 6
+#endif
+
+#ifdef LOGLEVEL_NOTICE
+#undef LOGLEVEL_NOTICE 5
+#endif
+
+#ifdef LOGLEVEL_WARNING
+#undef LOGLEVEL_WARNING 4
+#endif
+
+#ifdef LOGLEVEL_ERROR
+#undef LOGLEVEL_ERROR 3
+#endif
+
+#ifdef LOGLEVEL_CRITICAL
+#undef LOGLEVEL_CRITICAL 2
+#endif
+
+#ifdef	LOGLEVEL_ALERT
+#undef LOGLEVEL_ALERT 1
+#endif
+
+#ifdef LOGLEVEL_EMERGENCY
+#undef LOGLEVEL_EMERGENCY 0
+#endif
+}
+
--- Program/Server.c
+++ Program/Server.c
@@ -7,7 +7,6 @@
  * Project:		https://github.com/schoenm1/concurrent_c.git
  */
 
-
 /* ------------------ How to user this Program ------------------
  1) ...
  2) ...
@@ -16,40 +15,115 @@
 
  ---------------------------------------------------------------*/
 
-#include <assert.h>
-#include <ctype.h>
+#include <arpa/inet.h>  /* for sockaddr_in, inet_addr() and inet_ntoa() */
 #include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <signal.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/sem.h>
-#include <sys/msg.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/time.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>      /* for printf() and fprintf() and ... */
+#include <stdlib.h>     /* for atoi() and exit() and ... */
+#include <string.h>     /* for memset() and ... */
+#include <sys/socket.h> /* for socket(), bind(), recv, send(), and connect() */
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/ipc.h>
-#include <time.h>
-#include <unistd.h>
+#include <unistd.h>     /* for close() */
 
-#include <itskylib.h>	// exteral Library by lecturer "Karl Brodowsky"
-#include "Log-Level.h"	//defines the possibles LOG-LEVELS for output and if nessessary for writing to Logfile
+#include <itskylib.h>
+#include "Log-Level.h"
 
+#define RCVBUFSIZE 128   /* Size of receive buffer */
+#define MAXPENDING 5    /* Maximum outstanding connection requests */
+#define SERVERPORT_ARG "-p";
 
-
-
-
+unsigned short squareServPort;     /* Server port */
 
 
+
+
+struct validArgs {
+	int isSet;
+	char arg[];
+
+};
+
+struct validArgs validArguments[5];
+
+void initValidArguments(int argc, char *argv[]) {
+	int i;
+	int retcode;
+	char _logLevel[10] = LOGLEVEL_ARG;
+	char _serverPort[10] =SERVERPORT_ARG;
+	int counter_validArgs = 0;
+
+	/* Parse all Arguments (except arg[0]. This is File itself) */
+	for (i = 1; i < argc; i++) {
+		printf("Argument No. %i\t", i);
+		printf("Value = %s\n", argv[i]);
+
+		/* if "-l" is Arg, then set Log-Level */
+		if (strcmp(argv[i], _logLevel) == 0) {
+			printf("Treffer fuer Loglevel\n\n");
+			int _mylogLevel = (int) atoi(argv[i + 1]);
+			printf("Loglevel = %i\n", _mylogLevel);
+			retcode = setLogLevel(_mylogLevel);
+			handle_error(retcode, "LogLevel could not be set\n", PROCESS_EXIT);
+			i++;
+		}
+
+
+		/* parse for Server-Port */
+		if (strcmp(argv[i], _logLevel) == 0) {
+//hier fehlt noch die Zuweisung des Portes
+
+			i++;
+		}
+
+
+
+
+	}
+}
+void usage(const char *argv0, const char *msg) {
+	if (msg != NULL && strlen(msg) > 0) {
+		printf("%s\n\n", msg);
+	}
+	printf("Usage MUST\n==============\n");
+	printf("%s -p <Server Port>\n", argv0);
+	printf("Usage OPTIONAL\n====================\n");
+	printf("-l <int Loglevel> {0=EMERGENCY, 1=ALERT, INFORMATIONAL}\n");
+	exit(1);
+}
+
+void validatingArgs(int argc, char *argv[]) {
+	int i;
+
+	for (i = 0; i < argc; i++) {
+
+		//if (strcmp(thread_data_array[count2].filename, valid_file[count])
+
+
+	}
+
+}
 
 int main(int argc, char *argv[]) {
 
-	printf("debug test: %i",LOGLEVEL_ALERT);
+	initValidArguments(argc, argv);
+	//initValidArgs();
 
+	/*char valid_inputs[100][255];
+
+	 printf("\nValidating now the arguments and set the configuration of the Server...\n");
+	 printf("Loglevel = %i",(int) argv[2]);
+
+
+
+	 */
+
+	//setLogLevel(myint);
+
+
+	int retcode;
+	if (is_help_requested(argc, argv)) {
+		usage(argv[0], "");
+	}
 
 }
